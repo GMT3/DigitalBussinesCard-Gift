d3e078d2fc1fe37ad5ec5ebfa1cbdc93a32a1d3f
g1ftmkyelu <mkyelugift@gmail.com>
2023-01-14 09:28:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Gift</code> and save to reload.
+          Edit <code>Gift's</code> and save to reload.
         </p>
         <a
           className="App-link"
